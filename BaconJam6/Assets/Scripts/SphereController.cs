94fbd929da3a35072afdf88012457fffa7ca9c9d
sk-scd91 <scdeneen@gmail.com>
2013-10-26 17:11:07 -0400

    Now cube can trigger sphere.

---
+++ BaconJam6/Assets/Scripts/SphereController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// SphereController.cs
+
+public class SphereController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	
+	void OnTriggerEnter (Collider c) {
+		if (c.tag == "Player") {
+			gameObject.SetActive(false);
+		}
+	}
+}
